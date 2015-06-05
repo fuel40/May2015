9b1ce7cc9a76a844f216e872f9f925f880df7c4b
Nanda <nandakumarpurohit@gmail.com>
2015-06-04 21:20:03 -0400

    Added first day session

---
+++ 28May2015/DT.java
@@ -0,0 +1,22 @@
+class DT {
+
+	public static void main(String a[]) {
+	
+		float f1 = 12.25F;
+		double f2 = 34.45;
+
+		char ch = 'd';
+		int num = ch;
+		System.out.println(num); // 100
+
+		int x = 70;
+		char alpha = (char) x;
+		System.out.println(alpha);  // F
+	}
+}
+	
+
+
+
+
+
